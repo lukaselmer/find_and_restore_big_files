ca758e70f56e8ca6eee5138bb4bf1ce20c37039e
Lukas Elmer <lukas.elmer@gmail.com>
2014-08-13 17:14:48 +0200

    scaffold, refactoring

--- run.rb
+++ run.rb
@@ -1,56 +1,92 @@
 class Repo
+  def initialize(source_dir)
+    @source_dir = source_dir
+  end
 
+  def generate_paths
+    # code here
+  end
+
+  def generate_hashes
+    # code here
+  end
+
+  def checkout_older_revision!
+    # code here
+  end
+
+  def changed_files
+    # code here
+  end
+
+  def contains_older_revision?
+    # code here
+  end
 end
 
 class RepoFile
 
 end
 
-class Results
+class LostFiles
   @lost_files = []
+
+  def store(revision, file_path)
+    puts "#{file}: #{revision}"
+    @lost_files << [revision, file_path]
+  end
 
   def to_s
     @lost_files.map do |revision, file|
       "#{file} #{revision}"
     end.join("\n")
   end
+end
 
-  def store_lost_file(revision, file_path)
-    puts "#{file}: #{revision}"
-    @lost_files << [revision, file_path]
+class ResultTracker
+  def initialize(repo)
+    @repo = repo
+    @paths = repo.generate_paths
+    @hashes = repo.generate_hashes
+  end
+
+  def path_exists?(file)
+    @paths.include?(file.path)
+  end
+
+  def store(file)
+    @hashes << file.hash unless @hashes.include?(file.hash)
+    @paths << file.path unless @paths.include?(file.path)
+  end
+
+  def content_exists?(file)
+    # or, better: store hash s.t. compare_files(file1.hash, file2.hash) = true iff file1 is at least 90% similar to file2
+    @hashes.any? { |hash| file.hash == hash }
   end
 end
 
 source_dir = ENV['SOURCE_DIR']
-#out_dir = ENV['OUT_DIR']
 
 repo = Repo.new(source_dir)
-results = Results.new
+results = LostFiles.new
+result_tracker = ResultTracker.new(repo)
 
-paths = repo.generate_paths
-hashes = repo.generate_hashes
-# or, better: store hash s.t. compare_files(file1.hash, file2.hash) = true iff file1 is at least 90% similar to file2
 
-while (repo.contains_older_revision?) do
+while repo.contains_older_revision? do
   repo.checkout_older_revision!
   repo.changed_files.each do |file| # for every [changed|removed|added]
+    begin
+      # check if path in a newer version exists
+      next if result_tracker.path_exists?(file)
 
-    # check if path in a newer version exists
-    if paths.include?(file.path)
-      hashes << file.hash
-      next
+      # check if same / similar file exists
+      next if result_tracker.content_exists?(file)
+
+      # if algo gets to here, file is a lost file
+      results.store(repo.revision, file.path)
+    ensure
+      result_tracker.store(file)
     end
-
-    # check if same / similar file exists
-    if hashes.any?(-> (hash) { compare_files(file.hash, hash) })
-      paths << file.path
-      next
-    end
-
-    # if algo gets to here, file is a lost file
-    results.store_lost_file(repo.revision, file.path)
-    hashes << file.hash
-    paths << file.path
   end
 end
 
