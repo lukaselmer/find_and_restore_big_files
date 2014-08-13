76b6ab304786a8554fe19c279156e9cc5a8e4088
Lukas Elmer <lukas.elmer@gmail.com>
2014-08-13 18:44:56 +0200

    refactoring

--- lib/algo.rb
+++ lib/algo.rb
@@ -3,28 +3,26 @@ class Algo
 
   def initialize(source_dir)
     @source_dir = source_dir
-    @lost_files = nil
+    @repo = Repo.new(@source_dir)
+    @result_tracker = ResultTracker.new(@repo)
+    @lost_files = LostFiles.new
   end
 
   def run!
-    repo = Repo.new(@source_dir)
-    @lost_files = LostFiles.new
-    result_tracker = ResultTracker.new(repo)
-
-    while repo.contains_older_revision? do
-      repo.checkout_older_revision!
-      repo.changed_files.each do |file| # for every [changed|removed|added]
+    while @repo.contains_older_revision? do
+      @repo.checkout_older_revision!
+      @repo.changed_files.each do |file| # for every [changed|removed|added]
         begin
           # check if path in a newer version exists
-          next if result_tracker.path_exists?(file)
+          next if @result_tracker.path_exists?(file)
 
           # check if same / similar file exists
-          next if result_tracker.content_exists?(file)
+          next if @result_tracker.content_exists?(file)
 
           # if algo gets to here, file is a lost file
-          @lost_files.store(repo.revision, file.path)
+          @lost_files.store(@repo.revision, file.path)
         ensure
-          result_tracker.store(file)
+          @result_tracker.store(file)
         end
       end
     end
