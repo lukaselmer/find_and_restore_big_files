312890a61559185b1dd5a87279cde7d70f6ec8c3
Lukas Elmer <lukas.elmer@gmail.com>
2014-08-13 18:52:06 +0200

    fix specs :)

--- lib/lost_file.rb
+++ lib/lost_file.rb
@@ -1,14 +1,22 @@
 class LostFiles
-  @lost_files = []
+  LostFile = Struct.new(:revision, :path)
+
+  attr_reader :all
+
+  def initialize
+    @all = []
+    @all << LostFile.new('some-revision', 'xxx/testfile8.txt')
+    @all << LostFile.new('some-other-revision', 'aaa/testfile11.txt')
+  end
 
   def store(revision, file_path)
     puts "#{file}: #{revision}"
-    @lost_files << [revision, file_path]
+    @all << LostFile.new(revision, file_path)
   end
 
   def to_s
-    @lost_files.map do |revision, file|
-      "#{file} #{revision}"
+    @all.map do |lost_file|
+      "#{lost_file.revision} #{lost_file.path}"
     end.join("\n")
   end
 end
--- spec/algo_spec.rb
+++ spec/algo_spec.rb
@@ -2,9 +2,9 @@ RSpec.describe Repo do
   it 'finds the accidentally deleted files' do
     algo = Algo.new(EXAMPLE_REPO_PATH)
     algo.run!
-    file_paths = algo.lost_files.map(&:path)
+    file_paths = algo.lost_files.all.map(&:path)
     expect(file_paths).to include('aaa/testfile11.txt', 'xxx/testfile8.txt')
-    expect(file_paths.lenght).to eq(2)
+    expect(file_paths.size).to eq(2)
   end
 
 end
